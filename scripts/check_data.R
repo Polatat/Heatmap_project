c04103886c4bae7f15fa109c9d3146e2fff54dde
Polatat <jokerofblack2@gmail.com>
2025-02-27 00:41:13 +0700

    Upload data and some scripts

---
+++ scripts/check_data.R
@@ -0,0 +1,192 @@
+library(readxl)
+library(dplyr)
+
+# HEK293T_hACE2
+HEK293T_hACE2_sheet <- read_excel("data/41598_2021_96462_MOESM1_ESM.xls", 
+                                           col_types = c("text", "numeric", "numeric", 
+                                                         "numeric", "numeric", "numeric"))
+View(HEK293T_hACE2_sheet)
+
+HEK293T_hACE2_filtered_with_padj <- HEK293T_hACE2_sheet %>%
+                                    filter(padj < 0.05,
+                                    log2FoldChange >= 2 | log2FoldChange <= -1.5) 
+
+View(HEK293T_hACE2_filtered_with_padj)
+
+HEK293T_hACE2_filtered <- HEK293T_hACE2_sheet %>%
+                          filter(log2FoldChange >= 2 | log2FoldChange <= -1.5) 
+
+colnames(HEK293T_hACE2_filtered)[1] <- "Gene"
+View(HEK293T_hACE2_filtered)
+dim(HEK293T_hACE2_filtered) # 78 rows and 6 column
+
+
+gene_names <- HEK293T_hACE2_filtered$Gene
+
+filtered_gene_name <- unique(gene_names)
+filtered_gene_name
+
+filtered_gene <- c(
+  "TNFRSF9", "PTAFR", "IL23R", "CTH", "PTGER3", "LOC100131564",
+  "TBX15", "TXNIP", "MIR5087", "HSPA6", "HSPA7", "SLC9C2",
+  "PIGR", "FCAMR", "G0S2", "ATF3", "MRC1", "DDIT4",
+  "P4HA1", "LGI1", "CYP2C18", "CYP2C19", "SNHG1", "RAPGEF3",
+  "INHBE", "DDIT3", "SLC17A8", "IGF1", "PCK2", "DICER1-AS1",
+  "CHAC1", "ANPEP", "NUPR1", "MVP", "ANKRD26P1", "TVP23C",
+  "RDM1", "PECAM1", "SOX9-AS1", "CATSPERD", "KIAA1683", "LINC00663",
+  "FUT1", "ALLC", "ERICH2", "CPO", "TRIB3", "CEBPB-AS1",
+  "PCK1", "C1QTNF6", "ADM2", "LAMB2P1", "SLC7A11-AS1", "SLC7A11",
+  "NRN1", "SLC17A3", "HSPA1A", "HSPA1B", "C6orf48", "GCM1",
+  "FLJ46906", "FRMD1", "INHBA", "ASNS", "TFR2", "MUC17",
+  "DNAJB9", "MGAM", "MGAM2", "STC1", "SCARA5", "PPAPDC1B",
+  "LINC01301", "C8orf46", "ATP6V0D2", "TG", "VLDLR-AS1", "LURAP1L",
+  "IFNA22P", "TMEM215", "LINC00950", "ANXA1", "AKNA", "SAT1","NXF3"
+)
+
+#  A549
+A549_sheet <- read_excel("data/41598_2021_96462_MOESM1_ESM.xls", 
+                                           sheet = "A549",
+                                          col_types = c("text", "numeric", "numeric", 
+                                          "numeric", "numeric", "numeric"))
+View(A549_sheet)
+colnames(A549_sheet)[1] <- "Gene"
+
+
+A549_sheet_filtered <- A549_sheet %>%
+                       filter(Gene %in% filtered_gene)
+
+View(A549_sheet_filtered)
+
+
+
+#A549-hACE2
+
+A549_hACE2_sheet <- read_excel("data/41598_2021_96462_MOESM1_ESM.xls", 
+                         sheet = "A549-hACE2",
+                         col_types = c("text", "numeric", "numeric", 
+                                       "numeric", "numeric", "numeric"))
+
+View(A549_hACE2_sheet)
+colnames(A549_hACE2_sheet)[1] <- "Gene"
+
+A549_hACE2_sheet_filtered <- A549_hACE2_sheet%>%
+                            filter(Gene %in% filtered_gene)
+
+View(A549_hACE2_sheet_filtered)
+
+dim(A549_hACE2_sheet_filtered)
+
+
+# Caco2
+
+
+Caco2_sheet <- read_excel("data/41598_2021_96462_MOESM1_ESM.xls", 
+                               sheet = "Caco2",
+                               col_types = c("text", "numeric", "numeric", 
+                                             "numeric", "numeric", "numeric"))
+
+View(Caco2_sheet)
+colnames(Caco2_sheet)[1] <- "Gene"
+
+Caco2_sheet_filtered <- Caco2_sheet%>%
+                        filter(Gene %in% filtered_gene)
+
+View(Caco2_sheet_filtered)
+
+dim(Caco2_sheet_filtered)
+
+
+#Calu3_B
+
+
+Calu3_B_sheet <- read_excel("data/41598_2021_96462_MOESM1_ESM.xls", 
+                          sheet = "Calu3_B",
+                          col_types = c("text", "numeric", "numeric", 
+                                        "numeric", "numeric", "numeric"))
+
+View(Calu3_B_sheet)
+colnames(Calu3_B_sheet)[1] <- "Gene"
+
+Calu3_B_sheet_filtered <- Calu3_B_sheet%>%
+                          filter(Gene %in% filtered_gene)
+
+View(Calu3_B_sheet_filtered)
+
+dim(Calu3_B_sheet_filtered)
+
+
+#Calu3_C
+
+
+Calu3_C_sheet <- read_excel("data/41598_2021_96462_MOESM1_ESM.xls", 
+                            sheet = "Calu3_C",
+                            col_types = c("text", "numeric", "numeric", 
+                                          "numeric", "numeric", "numeric"))
+
+View(Calu3_C_sheet)
+colnames(Calu3_C_sheet)[1] <- "Gene"
+
+Calu3_C_sheet_filtered <- Calu3_C_sheet%>%
+                          filter(Gene %in% filtered_gene)
+
+View(Calu3_C_sheet_filtered)
+
+dim(Calu3_C_sheet_filtered)
+
+
+
+
+# hBO
+
+hBO_sheet <- read_excel("data/41598_2021_96462_MOESM1_ESM.xls", 
+                            sheet = "hBO",
+                            col_types = c("text", "numeric", "numeric", 
+                                          "numeric", "numeric", "numeric"))
+
+View(hBO_sheet)
+colnames(hBO_sheet)[1] <- "Gene"
+
+hBO_sheet_filtered <- hBO_sheet%>%
+  filter(Gene %in% filtered_gene)
+
+View(hBO_sheet_filtered)
+
+dim(hBO_sheet_filtered)
+
+
+
+#hAE
+
+
+hAE_sheet <- read_excel("data/41598_2021_96462_MOESM1_ESM.xls", 
+                        sheet = "hAE",
+                        col_types = c("text", "numeric", "numeric", 
+                                      "numeric", "numeric", "numeric"))
+
+View(hAE_sheet)
+colnames(hAE_sheet)[1] <- "Gene"
+
+hAE_sheet_filtered <- hAE_sheet%>%
+  filter(Gene %in% filtered_gene)
+
+View(hAE_sheet_filtered)
+
+dim(hAE_sheet_filtered)
+
+# hCM
+
+
+hCM_sheet <- read_excel("data/41598_2021_96462_MOESM1_ESM.xls", 
+                        sheet = "hCM",
+                        col_types = c("text", "numeric", "numeric", 
+                                      "numeric", "numeric", "numeric"))
+
+View(hCM_sheet)
+colnames(hCM_sheet)[1] <- "Gene"
+
+hCM_sheet_filtered <- hCM_sheet%>%
+                      filter(Gene %in% filtered_gene)
+
+View(hCM_sheet_filtered)
+
+dim(hCM_sheet_filtered)
