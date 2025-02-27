96f6d8a82d00a313c7e00a1781300e25b2eb6ae7
Polatat <jokerofblack2@gmail.com>
2025-02-27 17:47:13 +0700

    Add files via upload
    Upload scripts

--- scripts/check_data.R
+++ scripts/check_data.R
@@ -1,5 +1,7 @@
 library(readxl)
 library(dplyr)
+library(gplots)
+library(tibble)
 
 # HEK293T_hACE2
 HEK293T_hACE2_sheet <- read_excel("data/41598_2021_96462_MOESM1_ESM.xls", 
@@ -20,9 +22,14 @@ colnames(HEK293T_hACE2_filtered)[1] <- "Gene"
 View(HEK293T_hACE2_filtered)
 dim(HEK293T_hACE2_filtered) # 78 rows and 6 column
 
+# HEK293T_hACE2_log2FC
+HEK293T_hACE2_log2FC_filtered <- HEK293T_hACE2_filtered[,c("Gene","log2FoldChange")]
+colnames(HEK293T_hACE2_log2FC_filtered)[2] <- "HEK293T_hACE2"
+View(HEK293T_hACE2_log2FC_filtered)
 
+
+# filter gene
 gene_names <- HEK293T_hACE2_filtered$Gene
-
 filtered_gene_name <- unique(gene_names)
 filtered_gene_name
 
@@ -43,6 +50,10 @@ filtered_gene <- c(
   "IFNA22P", "TMEM215", "LINC00950", "ANXA1", "AKNA", "SAT1","NXF3"
 )
 
+A549_hACE2_log2FC_sheet <- A549_hACE2_sheet_filtered[,c("Gene","log2FoldChange")]
+
+View(A549_hACE2_log2FC_sheet)
+
 #  A549
 A549_sheet <- read_excel("data/41598_2021_96462_MOESM1_ESM.xls", 
                                            sheet = "A549",
@@ -57,6 +68,13 @@ A549_sheet_filtered <- A549_sheet %>%
 
 View(A549_sheet_filtered)
 
+#  A549_log2FC
+A549_sheet_log2Fc <- A549_hACE2_sheet_filtered %>%
+                      select(Gene,log2FoldChange)
+
+colnames(A549_sheet_log2Fc)[2] <- "A549"
+View(A549_sheet_log2Fc)
+
 
 
 #A549-hACE2
@@ -69,12 +87,21 @@ A549_hACE2_sheet <- read_excel("data/41598_2021_96462_MOESM1_ESM.xls",
 View(A549_hACE2_sheet)
 colnames(A549_hACE2_sheet)[1] <- "Gene"
 
+
 A549_hACE2_sheet_filtered <- A549_hACE2_sheet%>%
                             filter(Gene %in% filtered_gene)
+
 
 View(A549_hACE2_sheet_filtered)
 
 dim(A549_hACE2_sheet_filtered)
+
+#A549-hACE2_log2Foldchange
+A549_hACE2_log2FC_sheet <- A549_hACE2_sheet_filtered[,c("Gene","log2FoldChange")]
+
+colnames(A549_hACE2_log2FC_sheet)[2] <- "A549_A"
+View(A549_hACE2_log2FC_sheet)
+
 
 
 # Caco2
@@ -95,6 +122,14 @@ View(Caco2_sheet_filtered)
 
 dim(Caco2_sheet_filtered)
 
+# Caco2_log2FC
+
+Caco2_log2FC_sheet <- Caco2_sheet_filtered[,c("Gene","log2FoldChange")]
+
+colnames(Caco2_log2FC_sheet)[2] <- "Caco2"
+
+View(Caco2_log2FC_sheet)
+
 
 #Calu3_B
 
@@ -114,6 +149,13 @@ View(Calu3_B_sheet_filtered)
 
 dim(Calu3_B_sheet_filtered)
 
+# Calu3_B_log2FC
+Calu3_B_log2FC_sheet <- Calu3_B_sheet_filtered[,c("Gene","log2FoldChange")]
+
+colnames(Calu3_B_log2FC_sheet)[2] <- "Calu3_B"
+
+View(Calu3_B_log2FC_sheet)
+
 
 #Calu3_C
 
@@ -132,6 +174,15 @@ Calu3_C_sheet_filtered <- Calu3_C_sheet%>%
 View(Calu3_C_sheet_filtered)
 
 dim(Calu3_C_sheet_filtered)
+
+#Calu3_C_log2FC
+
+Calu3_C_log2FC_sheet <- Calu3_C_sheet_filtered[,c("Gene","log2FoldChange")]
+
+colnames(Calu3_C_log2FC_sheet)[2] <- "Calu3_C"
+
+View(Calu3_C_log2FC_sheet)
+
 
 
 
@@ -153,6 +204,17 @@ View(hBO_sheet_filtered)
 
 dim(hBO_sheet_filtered)
 
+# hBO_log2FC 
+
+hBO_log2FC_sheet <- hBO_sheet_filtered[,c("Gene","log2FoldChange")]
+
+colnames(hBO_log2FC_sheet)[2] <- "hBO"
+
+View(hBO_log2FC_sheet)
+
+
+
+
 
 
 #hAE
@@ -173,6 +235,15 @@ View(hAE_sheet_filtered)
 
 dim(hAE_sheet_filtered)
 
+# hAE_log2fc
+
+hAE_log2FC_sheet <- hAE_sheet_filtered[,c("Gene","log2FoldChange")]
+
+colnames(hAE_log2FC_sheet)[2] <- "hAE"
+
+View(hAE_log2FC_sheet)
+
+
 # hCM
 
 
@@ -190,3 +261,87 @@ hCM_sheet_filtered <- hCM_sheet%>%
 View(hCM_sheet_filtered)
 
 dim(hCM_sheet_filtered)
+
+# hCM_log2 fc
+
+hCM_log2FC_sheet <- hCM_sheet_filtered[,c("Gene","log2FoldChange")]
+
+colnames(hCM_log2FC_sheet)[2] <- "hCM"
+
+View(hCM_log2FC_sheet)
+
+
+
+# Merging table
+# function
+merge_table <- function(y){
+                               merge(heatmap_log2FC_table,
+                                y,
+                                by.x = "Gene",
+                                by.y = "Gene",
+                                all = TRUE)
+}
+# first merge
+heatmap_log2FC_table <- merge(x = HEK293T_hACE2_log2FC_filtered,
+                              y = A549_sheet_log2Fc,
+                              by.x = "Gene",
+                              by.y = "Gene",
+                              all = TRUE)
+View(heatmap_log2FC_table)
+#second merge
+heatmap_log2FC_table <- merge_table(A549_hACE2_log2FC_sheet)
+View(heatmap_log2FC_table)
+
+
+# third merge
+heatmap_log2FC_table <-merge_table(Caco2_log2FC_sheet)
+View(heatmap_log2FC_table)
+
+# fourth merge
+heatmap_log2FC_table <-merge_table(Calu3_B_log2FC_sheet)
+View(heatmap_log2FC_table)
+
+# fifth merge
+
+heatmap_log2FC_table <-merge_table(Calu3_C_log2FC_sheet)
+View(heatmap_log2FC_table)
+
+
+# sixth
+
+heatmap_log2FC_table <-merge_table(hBO_log2FC_sheet)
+View(heatmap_log2FC_table)
+
+
+#seven 
+
+heatmap_log2FC_table <-merge_table(hAE_log2FC_sheet)
+View(heatmap_log2FC_table)
+
+#eigth
+
+
+heatmap_log2FC_table <-merge_table(hCM_log2FC_sheet)
+View(heatmap_log2FC_table)
+
+# heatmap
+
+
+new_heatmap_log2FC_table <- column_to_rownames(heatmap_log2FC_table,
+                                                      var = "Gene")
+View(new_heatmap_log2FC_table)
+
+heatmap_log2FC_table_matrix <- as.matrix(new_heatmap_log2FC_table)
+
+View(heatmap_log2FC_table_matrix)
+
+dim(heatmap_log2FC_table_matrix)
+
+
+# heatmap
+
+heatmap.2(heatmap_log2FC_table_matrix)
+# column  to rownames
+
+
+
